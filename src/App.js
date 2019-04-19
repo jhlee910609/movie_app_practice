1d219cdb24451914bb5419e0ab3e4768c110c47f
JunHee Lee <jhlee910609@gmail.com>
2019-04-19 09:08:34 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Movie from './Movie';
 
 class App extends Component {
+
+  state = {}
+
+  componentDidMount() {
+    this._getMovies();
+  }
+
+  _renderMovies = () => {
+    const movies = this.state.movies.map((movie) => {
+      return <Movie
+        title={movie.title_english}
+        poster={movie.medium_cover_image}
+        key={movie.id}
+        genres={movie.genres}
+        synopsis={movie.synopsis} />
+    })
+    return movies;
+  }
+
+  // 비동기 코드
+  _getMovies = async () => {
+    // await : this._callApi() 가 끝날 때까지 동작 x
+    const movies = await this._callApi()
+    this.setState({
+      movies
+
+    })
+
+  }
+
+  _callApi = () => {
+    return fetch("https://yts.am/api/v2/list_movies.json?sort_by=download_count")
+      .then(response => response.json())
+      .then(json => json.data.movies)
+      .catch(err => console.log(err))
+
+    // // 과거
+    // fetch("https://yts.am/api/v2/list_movies.json?sort_by=rating")
+    // .then()
+    // .catch(function(err){
+    //   console.log(err)
+    // })
+  }
+
   render() {
+    const {movies} = this.state
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      // jsx : Simpel HTML 
+      <div className={movies ? "App" : "App--loading"}>
+        {movies ? this._renderMovies() : "Loading"}
       </div>
     );
   }
