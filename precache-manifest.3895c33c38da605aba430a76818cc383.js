295f91b5b4444fca75379c7f8d3101f8cb2b2345
JunHee Lee <jhlee910609@gmail.com>
2019-04-19 09:09:54 +0900

    Updates

---
+++ precache-manifest.3895c33c38da605aba430a76818cc383.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f56cbfd09050d46037d5",
+    "url": "/myapp/static/js/runtime~main.e4f53ea3.js"
+  },
+  {
+    "revision": "0a47d9095d316c769c95",
+    "url": "/myapp/static/js/main.e5d2c244.chunk.js"
+  },
+  {
+    "revision": "74286a96ace76faac2ea",
+    "url": "/myapp/static/js/2.5425dfe7.chunk.js"
+  },
+  {
+    "revision": "0a47d9095d316c769c95",
+    "url": "/myapp/static/css/main.38469a30.chunk.css"
+  },
+  {
+    "revision": "45dc7272b1f617bd9c1fe992484a1939",
+    "url": "/myapp/index.html"
+  }
+];
